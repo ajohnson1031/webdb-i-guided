98c8ea7f47076ae1952d65a74d6b2f77ead3cb5a
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-11-13 11:11:15 -0500

    completed posts endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,82 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db("posts");
+    posts.length
+      ? res.status(200).json(posts)
+      : res.status(404).json({ message: "invalid query, please try again" });
+  } catch {
+    res
+      .status(500)
+      .json({ message: "could not retrieve records from the database" });
+  }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await db("posts").where({ id: id });
+    post.length
+      ? res.status(200).json(post)
+      : res.status(404).json({ message: "invalid id" });
+  } catch {
+    res
+      .status(500)
+      .json({ message: "could not retrieve this record from the database" });
+  }
 });
 
-router.post('/', (req, res) => {
-
+router.post("/", async (req, res) => {
+  const body = req.body;
+  if (!body.title || !body.contents)
+    res
+      .status(404)
+      .json({ message: "POST request must have both title and contents" });
+  else {
+    try {
+      const post = await db.insert(body).into("posts");
+      res.status(201).json(post);
+    } catch (err) {
+      res.status(500).json({ message: "db problem: ", err: err });
+    }
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
+  try {
+    const postUpdate = await db("posts")
+      .where({ id })
+      .update(changes);
+    postUpdate === 1
+      ? res.status(200).json({ message: "Post successfuly updated!" })
+      : res.status(404).json({ message: "Invalid ID" });
+  } catch (err) {
+    res.status(500).json({ message: "db problem", err: err });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const deletePost = await db("posts")
+      .where({ id })
+      .del();
+    deletePost
+      ? res.status(200).json({ message: "Post successfully deleted." })
+      : res.status(400).json({ message: "No post with that ID exists." });
+  } catch (err) {
+    res.status(500).json({ message: "db problem: ", err });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require('express');
+const express = require("express");
 
-const PostRouter = require('./posts/post-router.js');
+const PostRouter = require("./posts/post-router.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/posts', PostRouter);
+server.use("/api/posts", PostRouter);
 
-server.get('/', (req, res) => {
-  res.send('<h3>DB Helpers with knex</h3>');
+server.get("/", PostRouter, (req, res) => {
+  res.send("<h3>DB Helpers with knex</h3>");
+});
+
+server.get("/:id", PostRouter, (req, res) => {
+  res.send("<p>Because knex is the kbest!<p>");
+});
+
+server.post("/", PostRouter, (req, res) => {
+  res.send("<p>Knexium! Now treating extreme flatulence.<p>");
+});
+
+server.put("/:id", PostRouter, (req, res) => {
+  res.send("<p>Side effects include json responses.<p>");
+});
+
+server.delete("/:id", PostRouter, (req, res) => {
+  res.send("<p>Ask your developer if knex is right for you.<p>");
 });
 
 module.exports = server;
